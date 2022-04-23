49fdf1678721e5312bcfd7d93312afff81d2ed60
ghida5130 <ghida5130@gmail.com>
2022-04-24 04:51:20 +0900

    ??

--- javatest/src/javatest/PushPrint.java
+++ javatest/src/javatest/PushPrint.java
@@ -3,10 +3,7 @@ package javatest;
 public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Add Java file");
-<<<<<<< HEAD
 		System.out.println("Hello in Local...");
-=======
 		System.out.println("Hello...");
->>>>>>> refs/remotes/origin/master
 	}
 }
