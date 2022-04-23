2df0b5eed1bdb7ccb1d43837652bf9f1d5179599
ghida5130 <ghida5130@gmail.com>
2022-04-24 04:45:29 +0900

    added PushPrint.java

--- javatest/src/javatest/HelloWorld.java
+++ javatest/src/javatest/HelloWorld.java
@@ -5,10 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello his World");
-<<<<<<< HEAD
 		System.out.println("Hello your World");
-=======
 		System.out.println("Hello her World");
->>>>>>> refs/heads/hotfix
 	}
 }
